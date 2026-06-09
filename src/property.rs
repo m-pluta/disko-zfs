bfad44189d1ab8e8d4199c950d1d85a451f9c355
ldesgoui <ldesgoui@gmail.com>
2026-06-09 18:23:49 +0200

    property: change PropertyValue::Integer into ::Number
    There exists no settable properties in the OpenZFS codebase
    that are transmitted as i64, however the numeric ones are all u64.
    Some read-only properties can exist as negative numbers,
    user-defined properties always are strings.

--- src/property.rs
+++ src/property.rs
@@ -41,27 +41,16 @@ impl PropertySource {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum PropertyValue {
-    Integer(i64),
+    Number(u64),
     String(String),
 }
 
 impl PropertyValue {
     pub fn to_string(&self) -> String {
         match self {
-            PropertyValue::Integer(int) => int.to_string(),
+            PropertyValue::Number(num) => num.to_string(),
             PropertyValue::String(string) => string.clone(),
         }
-    }
-
-    pub fn new_string<S>(str: S) -> PropertyValue
-    where
-        S: AsRef<str>,
-    {
-        PropertyValue::String(str.as_ref().to_owned())
-    }
-
-    pub fn new_integer(integer: i64) -> PropertyValue {
-        PropertyValue::Integer(integer)
     }
 }
 
@@ -71,21 +60,14 @@ impl<'de> Visitor<'de> for PropertyValueVisitor {
     type Value = PropertyValue;
 
     fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        formatter.write_str("either a integer or string")
-    }
-
-    fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
-    where
-        E: serde::de::Error,
-    {
-        Ok(Self::Value::Integer(v))
+        formatter.write_str("either a numeric value or a string")
     }
 
     fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
     where
         E: serde::de::Error,
     {
-        Ok(Self::Value::Integer(v as i64))
+        Ok(Self::Value::Number(v))
     }
 
     fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
@@ -111,7 +93,7 @@ impl Serialize for PropertyValue {
         S: serde::Serializer,
     {
         match self {
-            PropertyValue::Integer(int) => int.serialize(serializer),
+            PropertyValue::Number(num) => num.serialize(serializer),
             PropertyValue::String(str) => str.serialize(serializer),
         }
     }
